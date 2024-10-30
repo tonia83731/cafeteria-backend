377dd9489450bd81b84850c9416519c07c6f378b
huiyu.tonia <huiyu.tonia.chang@gmail.com>
2024-10-30 10:25:46 +0800

    feat: initialized backend project

---
+++ app.js
@@ -0,0 +1,24 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+module.exports = app;
