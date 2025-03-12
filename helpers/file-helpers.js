30170a83805f13a3ce352bbec65ad3ec7c8799b5
huiyu.tonia <huiyu.tonia.chang@gmail.com>
2025-03-12 16:34:47 +0800

    feat: test multer with memory storage

--- helpers/file-helpers.js
+++ helpers/file-helpers.js
@@ -3,16 +3,14 @@ const imgur = require("imgur");
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID;
 imgur.setClientId(IMGUR_CLIENT_ID);
 
-console.log(IMGUR_CLIENT_ID);
-
 const imgurFileHandler = (file) => {
   return new Promise((resolve, reject) => {
     if (!file) return resolve(null);
 
-    console.log(file.path);
+    const base64 = file.buffer.toString("base64");
 
     imgur
-      .uploadFile(file.path)
+      .uploadBase64(base64) // Upload the image directly from buffer
       .then((img) => {
         if (img && img.link) {
           resolve(img.link);
@@ -24,6 +22,20 @@ const imgurFileHandler = (file) => {
         console.error("Imgur upload failed:", err);
         reject(err);
       });
+
+    // imgur
+    //   .uploadFile(file.path)
+    //   .then((img) => {
+    //     if (img && img.link) {
+    //       resolve(img.link);
+    //     } else {
+    //       reject(new Error("Image upload failed, no link received."));
+    //     }
+    //   })
+    //   .catch((err) => {
+    //     console.error("Imgur upload failed:", err);
+    //     reject(err);
+    //   });
   });
 };
 
--- middleware/multer.js
+++ middleware/multer.js
@@ -1,3 +1,7 @@
 const multer = require("multer");
-const upload = multer({ dest: "temp/" });
+// const upload = multer({ dest: "temp/" });
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
 module.exports = upload;
