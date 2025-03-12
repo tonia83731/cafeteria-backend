956baf533a690fa2e54f5c01233b039dd73c6086
huiyu.tonia <huiyu.tonia.chang@gmail.com>
2025-03-12 16:49:28 +0800

    feat: use temp to save image

--- helpers/file-helpers.js
+++ helpers/file-helpers.js
@@ -1,18 +1,27 @@
 const imgur = require("imgur");
 
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID;
+// console.log(IMGUR_CLIENT_ID);
 imgur.setClientId(IMGUR_CLIENT_ID);
+
+// const imgurFileHandler = (file) => {
+//   return new Promise((resolve, reject) => {
+//     if (!file) return resolve(null);
+//     return imgur
+//       .uploadFile(file.path)
+//       .then((img) => {
+//         resolve(img?.link || null); // 檢查 img 是否存在
+//       })
+//       .catch((err) => reject(err));
+//   });
+// };
 
 const imgurFileHandler = (file) => {
   return new Promise((resolve, reject) => {
     if (!file) return resolve(null);
 
-    const base64 = file.buffer.toString("base64");
-
-    console.log(file, base64);
-
     imgur
-      .uploadBase64(base64) // Upload the image directly from buffer
+      .uploadFile(file.path)
       .then((img) => {
         if (img && img.link) {
           resolve(img.link);
@@ -24,20 +33,6 @@ const imgurFileHandler = (file) => {
         console.error("Imgur upload failed:", err);
         reject(err);
       });
-
-    // imgur
-    //   .uploadFile(file.path)
-    //   .then((img) => {
-    //     if (img && img.link) {
-    //       resolve(img.link);
-    //     } else {
-    //       reject(new Error("Image upload failed, no link received."));
-    //     }
-    //   })
-    //   .catch((err) => {
-    //     console.error("Imgur upload failed:", err);
-    //     reject(err);
-    //   });
   });
 };
 
--- middleware/multer.js
+++ middleware/multer.js
@@ -1,7 +1,3 @@
 const multer = require("multer");
-// const upload = multer({ dest: "temp/" });
-
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
-
+const upload = multer({ dest: "temp/" });
 module.exports = upload;
